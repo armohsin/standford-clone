aebba4d9cc9a5e415ea6f68c887a8c8bc8218c2a
armohsin <89929080+armohsin@users.noreply.github.com>
2022-10-24 14:12:58 +0500

    1st commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  ScrollView, StyleSheet, Text, View } from 'react-native';
 
+import Index from './Index'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <ScrollView style={{width:'100%'}} >
+      <Index/>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
